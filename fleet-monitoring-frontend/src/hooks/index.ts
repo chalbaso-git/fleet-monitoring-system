6a8446dae9632c41880536eff51d072b8ec557c9
Alba Sosa, César Humberto <c.albasosa@dxc.com>
2025-07-20 21:20:32 -0500

    Eliminación de archivos innecesarios para la configuración, actualización de package

--- fleet-monitoring-frontend/src/hooks/index.ts
+++ fleet-monitoring-frontend/src/hooks/index.ts
@@ -1,6 +1,2 @@
-// Export all API hooks
-export * from './api/alert';
-export * from './api/geolocation';
-export * from './api/route';
-export * from './api/vehicle';
-export * from './api/routing';
+﻿// Export all available hooks
+export * from './useAlerts';
